386e3c084228224e8985345d3c2c13d02a13d03a
Marshall Culpepper <mculpepper@appcelerator.org>
2008-09-10 04:41:27 +0000

    files needed for the final .plugin, including a script to generate and copy for testing

---
+++ plugin/titanium_plugin/English.lproj/Localized.r
@@ -0,0 +1,15 @@
+#include <CoreServices/CoreServices.r>
+
+resource 'STR#' (126) { {
+    "Simple Netscape plug-in that displays movies",
+    "Netscape Movie Plug-In"
+} };
+
+resource 'STR#' (127) { {
+    "QuickTime Movie"
+} };
+
+resource 'STR#' (128) { {
+    "video/x-netscapemovie",
+    "mov"
+} };
---
+++ plugin/titanium_plugin/generate_plugin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PLUGIN_NAME=Titanium
+PLUGIN_DIR=build/$PLUGIN_NAME.plugin
+
+mkdir $PLUGIN_DIR
+mkdir $PLUGIN_DIR/Contents
+mkdir $PLUGIN_DIR/Contents/MacOS
+mkdir $PLUGIN_DIR/Contents/Resources
+
+cd $PLUGIN_DIR/Contents
+cp -r ../../../English.lproj Resources
+cp ../../../Info.plist ../../../version.plist .
+cp ../../../scintilla/libscintilla.a MacOS
+cp ../../Debug/libtitanium_plugin.dylib "MacOS/$PLUGIN_NAME"
+
+cd ../../..
+cp -r $PLUGIN_DIR ~/Library/Internet\ Plug-ins/
+
+rm -rf $PLUGIN_DIR
