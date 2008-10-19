37a993de4ef2e0d42dd447107b06fe48713d2994
Marshall Culpepper <mculpepper@appcelerator.org>
2008-10-19 04:12:58 +0000

    run.sh now runs the webkit_shell, run_safari.sh runs in safari

--- app/run.sh
+++ app/run.sh
@@ -5,5 +5,9 @@ rm -rf ~/Library/Internet\ Plug-Ins/Titanium.plugin
 cd ../plugin/titanium_plugin
 ./generate_plugin.sh
 
-cd ../../app
-/Applications/Safari.app/Contents/MacOS/Safari titanium.html
+#cd ../../app
+#/Applications/Safari.app/Contents/MacOS/Safari titanium.html
+
+cd ../../webkit_shell/osx/build/Debug/webkit_shell.app/Contents/MacOS/
+
+./webkit_shell 'http://localhost/titanium/titanium.html'
