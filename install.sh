59eb5c93a3f74cae022432223c9c3c45e09c27a2
Marshall Culpepper <marshall.law@gmail.com>
2008-10-28 14:23:59 -0500

    hopefully last of fixes to install.sh.. use full path when linking, and add -f so as to forcefully override old links

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 # must be root!
 
 APPC_PATH=/Library/Appcelerator
-
+PWD=`pwd`
 mkdir $APPC_PATH/titanium
 mkdir $APPC_PATH/titanium/plugins
 
-ln -s sdk/support $APPC_PATH/titanium/support
-ln -s sdk/lib/launcher.rb $APPC_PATH/lib/launcher.rb
-ln -s sdk/lib/packager.rb $APPC_PATH/lib/packager.rb
-ln -s sdk/commands/launch_project.rb $APPC_PATH/commands/launch_project.rb
-ln -s sdk/commands/package_project.rb $APPC_PATH/commands/package_project.rb
-ln -s runtime/plugins/tiplugin.rb $APPC_PATH/titanium/tiplugin.rb
-ln -s runtime/src/titanium.js $APPC_PATH/titanium/titanium.js
-ln -s runtime/webkit_shell $APPC_PATH/titanium/webkit_shell
+ln -fs $PWD/sdk/support $APPC_PATH/titanium/support
+ln -fs $PWD/sdk/lib/launcher.rb $APPC_PATH/lib/launcher.rb
+ln -fs $PWD/sdk/lib/packager.rb $APPC_PATH/lib/packager.rb
+ln -fs $PWD/sdk/commands/launch_project.rb $APPC_PATH/commands/launch_project.rb
+ln -fs $PWD/sdk/commands/package_project.rb $APPC_PATH/commands/package_project.rb
+ln -fs $PWD/sdk/commands/install_tiplugin.rb $APPC_PATH/commands/install_tiplugin.rb
+ln -fs $PWD/runtime/plugins/tiplugin.rb $APPC_PATH/titanium/tiplugin.rb
+ln -fs $PWD/runtime/src/titanium.js $APPC_PATH/titanium/titanium.js
+ln -fs $PWD/runtime/webkit_shell $APPC_PATH/titanium/webkit_shell
 
 cd runtime/webkit_shell/osx
 xcodebuild
