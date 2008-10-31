a6af3d033d1c714188a532fbe13ed78d6495cd10
Marshall Culpepper <marshall.law@gmail.com>
2008-10-31 13:42:52 -0500

    updated to add new lib script, remove unnecessary linking

--- install.sh
+++ install.sh
@@ -3,18 +3,13 @@
 
 APPC_PATH=/Library/Appcelerator
 PWD=`pwd`
-mkdir $APPC_PATH/titanium
-mkdir $APPC_PATH/titanium/plugins
 
-sudo ln -fs $PWD/sdk/support $APPC_PATH/titanium/support
 sudo ln -fs $PWD/sdk/lib/launcher.rb $APPC_PATH/lib/launcher.rb
 sudo ln -fs $PWD/sdk/lib/packager.rb $APPC_PATH/lib/packager.rb
+sudo ln -fs $PWD/sdk/lib/titanium.rb $APPC_PATH/lib/titanium.rb
 sudo ln -fs $PWD/sdk/commands/launch_project.rb $APPC_PATH/commands/launch_project.rb
 sudo ln -fs $PWD/sdk/commands/package_project.rb $APPC_PATH/commands/package_project.rb
 sudo ln -fs $PWD/sdk/commands/install_tiplugin.rb $APPC_PATH/commands/install_tiplugin.rb
-sudo ln -fs $PWD/runtime/plugins/tiplugin.rb $APPC_PATH/titanium/tiplugin.rb
-sudo ln -fs $PWD/runtime/src/titanium.js $APPC_PATH/titanium/titanium.js
-sudo ln -fs $PWD/runtime/webkit_shell $APPC_PATH/titanium/webkit_shell
 
 cd runtime/webkit_shell/osx
 xcodebuild
