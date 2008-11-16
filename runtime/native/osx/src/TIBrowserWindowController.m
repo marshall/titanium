6e9cf69b3ecc198664962c4c8ef8a22f98ee75c6
Jeff Haynie <jhaynie@gmail.com>
2008-11-15 19:23:20 -0800

    cleanup boiler plates, fixed copyright and added version

--- runtime/native/osx/src/TIBrowserWindowController.m
+++ runtime/native/osx/src/TIBrowserWindowController.m
@@ -1,9 +1,21 @@
-//
-//  TIBrowserWindowController.m
-//  Titanium
-//
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
+/**
+ * This file is part of Appcelerator's Titanium project.
+ *
+ * Copyright 2008 Appcelerator, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
 
 #import "TIBrowserWindowController.h"
 #import "TIAppDelegate.h"
@@ -102,8 +114,12 @@ typedef enum {
 
 - (void)customizeUserAgent {
 	// produces something like:
-	// Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Appcelerator Titanium
-	[webView setApplicationNameForUserAgent:@"Appcelerator Titanium"];	
+	// Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Titanium/1.0
+	NSString *shortVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+	if (!shortVersion) 
+		shortVersion = @"0.0";
+	NSString *version = [NSString stringWithFormat:@"Titanium/%@", shortVersion];
+	[webView setApplicationNameForUserAgent:version];	
 }
 
 
