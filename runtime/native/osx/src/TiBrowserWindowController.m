79de130b6800bc1f3504cc3ba943e214077c4ec0
Marshall Culpepper <marshall.law@gmail.com>
2008-11-18 14:44:34 -0600

    slightly modified public API.. TiNative->ti, and singleton objects are field accessors on the top level now i.e. TiNative.getApp() -> ti.App. Also menu/window functions moved to their own factories... ti.Window.createWindow, etc. Updated index.html to show changes.

--- runtime/native/osx/src/TiBrowserWindowController.m
+++ runtime/native/osx/src/TiBrowserWindowController.m
@@ -20,7 +20,7 @@
 #import "TiBrowserWindowController.h"
 #import "TiAppDelegate.h"
 #import "TiBrowserDocument.h"
-#import "TiNative.h"
+#import "TiObject.h"
 #import "TiJavaScriptPromptWindowController.h"
 #import "TiProtocol.h"
 #import "TiBrowserWindow.h"
@@ -215,11 +215,11 @@ typedef enum {
 - (void)webView:(WebView *)wv windowScriptObjectAvailable:(WebScriptObject *)windowScriptObject 
 {
 	if (wv != webView) return;
-	TiNative *tiNative = [[TiNative alloc] initWithWebView:webView];
-	[windowScriptObject setValue:tiNative forKey:@"TiNative"];
+	TiObject *tiObject = [[TiObject alloc] initWithWebView:webView];
+	[windowScriptObject setValue:tiObject forKey:@"ti"];
 //	[javaScriptObject include:@"titanium/titanium.js"];
 //	[javaScriptObject include:@"titanium/plugins.js"];
-	[tiNative release];
+	[tiObject release];
 }
 
 
