55d25ff17453e21df9725c9934082d4f46a4f092
Jorge Servin <servin7@gmail.com>
2008-11-23 15:28:26 -0600

    add new ti_app files

---
+++ runtime/native/win32/ti_shell/src/ti_app.cc
@@ -0,0 +1,43 @@
+/*
+* Copyright 2006-2008 Appcelerator, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "ti_app.h"
+
+#include "ti_web_shell.h"
+
+TiApp::TiApp(TiWebShell *tiWebShell)
+{
+	this->tiWebShell = tiWebShell;
+
+	BindMethod("show", &TiApp::show);
+	BindMethod("hide", &TiApp::hide);
+}
+
+TiApp::~TiApp()
+{
+}
+
+void TiApp::hide (const CppArgumentList &args, CppVariant *result)
+{
+	this->tiWebShell->showWindow(SW_HIDE);
+	this->tiWebShell->createTrayIcon();
+}
+
+void TiApp::show (const CppArgumentList &args, CppVariant *result)
+{
+	this->tiWebShell->showWindow(SW_SHOW);
+	this->tiWebShell->removeTrayIcon();
+}
---
+++ runtime/native/win32/ti_shell/src/ti_app.h
@@ -0,0 +1,35 @@
+/*
+* Copyright 2006-2008 Appcelerator, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#ifndef TI_APP_H_
+#define TI_APP_H_
+
+#include "webkit/glue/cpp_bound_class.h"
+#include "webkit/glue/webview.h"
+
+class TiWebShell;
+
+class TiApp : public CppBoundClass
+{
+	TiWebShell *tiWebShell;
+public:
+	TiApp(TiWebShell *tiWebShell);
+	~TiApp(void);
+
+	void hide (const CppArgumentList &args, CppVariant *result);
+	void show (const CppArgumentList &args, CppVariant *result);
+};
+
+#endif // TI_APP_H define
