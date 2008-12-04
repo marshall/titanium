b0d29f1e08381cb78bc596efa2c12b4cfd1a01c1
Marshall Culpepper <mculpepper@appcelerator.org>
2008-12-04 11:26:54 -0800

    updated menu suite to renamed menu factory methods, added more gears tests

--- runtime/tests/apivalidator/public/tests/menu.js
+++ runtime/tests/apivalidator/public/tests/menu.js
@@ -3,8 +3,32 @@ testSuite("ti.Menu API tests", "dummy.html", {
 		test("top level API", function() {
 			assert(ti != null);
 			assert(ti.Menu != null);
-			assert(ti.Menu.createSystemMenu != null);
-			assert(ti.Menu.createUserMenu != null);
+			assert(ti.Menu.createAppMenu != null);
+			assert(ti.Menu.createTrayMenu != null);
+		});
+
+		test("menu object API", function() {
+			assert(ti != null);
+			assert(ti.Menu != null);
+
+			var menu = ti.Menu.createAppMenu("apivalidator");
+			assert(menu != null);
+			assert(menu.addItem != null);
+			assert(menu.addSeparator != null);
+			assert(menu.addSubMenu != null);
+
+			var submenu = menu.addSubMenu("submenu");
+			assert(submenu != null);
+			assert(submenu.addItem != null);
+			assert(submenu.addSeparator != null);
+			assert(submenu.addSubMenu != null);
+
+			menu = ti.Menu.createTrayMenu("app://tests/test.ico", "Caption", function() {});
+			assert(menu != null);
+			assert(menu.addItem != null);
+			assert(menu.addSeparator != null);
+			assert(menu.addSubMenu != null);
+			
 		});
 	}
 });
