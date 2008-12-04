8c06766d64a71c3bc82b74d97d98186fa11a5f3a
Jeff Haynie <jhaynie@gmail.com>
2008-12-04 01:11:12 -0800

    initial stab at user menus, more work needed for finding and inserting

--- runtime/native/osx/src/TiMenuAction.m
+++ runtime/native/osx/src/TiMenuAction.m
@@ -15,8 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
-
+#import <WebKit/WebKit.h>
 #import "TiMenuAction.h"
+#import "TiController.h"
 
 @implementation TiMenuAction
 
@@ -26,27 +27,134 @@
 	target=nil;
 	[title release];
 	title=nil;
+	[menu release];
+	menu=nil;
 	[super dealloc];
 }
 
 
-- (TiMenuAction*)initWithFunc:(WebScriptObject*)f title:(NSString*)t
+- (TiMenuAction*)initWithFunc:(WebScriptObject*)f title:(NSString*)t menu:(NSMenu*)m
 {
 	self = [super init];
-	target = f;
-	title = t;
-	[target retain];
-	[title retain];
+	if (self != nil)
+	{
+		target = f;
+		title = t;
+		menu = m;
+		if (target) 
+		{
+			[target retain];
+		}
+		[title retain];
+		[menu retain];
+	}
 	return self;
 }
 
 - (void)execute
 {
 	NSMutableArray *result = [[NSMutableArray alloc] init];
-	[result addObject:target]; // scope
-	[result addObject:title]; // argument
-	[target callWebScriptMethod:@"call" withArguments:result];
+	if (target)
+	{
+		[result addObject:target]; // scope
+		[result addObject:title]; // argument
+		[target callWebScriptMethod:@"call" withArguments:result];
+	}
 	[result release];
+}
+
+- (void)setTitle:(NSString*)t
+{
+	[title release];
+	title = t;
+	[title retain];
+	[super setTitle:t];
+}
+
+-(NSString*)getTitle
+{
+	return title;
+}
+
+-(void)setEnabled:(BOOL)yn
+{
+	[super setEnabled:yn];
+}
+
+-(BOOL)isEnabled
+{
+	return [super isEnabled];
+}
+
+-(void)setVisible:(BOOL)yn
+{
+	[super setHidden:!yn];
+}
+
+-(BOOL)isVisible
+{
+	return ![super isHidden];
+}
+
+-(void)setIcon:(NSString*)u 
+{
+	NSURL *url = [TiController formatURL:u];
+	NSImage *image = [[NSImage alloc] initWithContentsOfURL:url];
+	[super setImage:image];
+}
+
+-(void)setCallback:(WebScriptObject*)f
+{
+	[target release];
+	target = f;
+	[target retain];
+}
+
+#pragma mark -
+#pragma mark WebScripting
+
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)sel {
+	return (nil == [self webScriptNameForSelector:sel]);
+}
+
+
++ (NSString *)webScriptNameForSelector:(SEL)sel 
+{
+	if (sel == @selector(setEnabled:)) {
+		return @"setEnabled";
+	}
+	else if (sel == @selector(isEnabled)) {
+		return @"isEnabled";
+	}
+	else if (sel == @selector(isVisible)) {
+		return @"isVisible";
+	}
+	else if (sel == @selector(setVisible:)) {
+		return @"setVisible";
+	}
+	else if (sel == @selector(setIcon:)) {
+		return @"setIcon";
+	}
+	else if (sel == @selector(setCallback:)) {
+		return @"setAction";
+	}
+	else if (sel == @selector(setTitle:)) {
+		return @"setTitle";
+	}
+	else if (sel == @selector(title)) {
+		return @"getTitle";
+	}
+	return nil;
+}
+
+
++ (BOOL)isKeyExcludedFromWebScript:(const char*)key {
+	return (nil == [self webScriptNameForKey:key]);
+}
+
+
++ (NSString *)webScriptNameForKey:(const char *)name {
+	return nil;
 }
 
 @end
--- runtime/native/osx/src/TiUserMenu.m
+++ runtime/native/osx/src/TiUserMenu.m
@@ -15,27 +15,114 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
-
+#import <WebKit/WebKit.h>
 #import "TiUserMenu.h"
+#import "TiMenuAction.h"
 
 
 @implementation TiUserMenu
 
-- (id)init 
+-(id)initWithMenu:(NSMenu*)m
 {
 	self = [super init];
-	if (self) {
+	if (self) 
+	{
+		menu = m;
+		[menu retain];
 	}
 	return self;
 }
 
 - (void)dealloc
 {
+	[items removeAllObjects];
+	[items release];
+	items = nil;
+	[menu release];
+	menu = nil;
 	[super dealloc];
 }
 
 - (NSString *)description {
 	return @"[TiUserMenu native]";
+}
+
+- (void)setTitle:(NSString*)title
+{
+	[menu setTitle:title];
+}
+
+- (NSString*)getTitle
+{
+	return [menu title];
+}
+
+- (void)itemClicked:(id)sender
+{
+	[sender execute];
+}
+
+- (TiMenuAction*)addItem:(NSString*)name f:(WebScriptObject*)f
+{
+	WebScriptObject *fn = [f isEqual:[WebUndefined undefined]] ? nil : f;
+	TiMenuAction *action = [[TiMenuAction alloc] initWithFunc:fn title:name menu:menu];
+	[items addObject:action];
+	[action setAction:@selector(itemClicked:)];
+	[action setTitle:name];
+	[action setEnabled:true];
+	[action setTarget:self];
+	[menu addItem:action];
+	return action;
+}
+
+- (NSMenuItem*)addSeparator
+{
+	NSMenuItem *sep = [NSMenuItem separatorItem];
+	[menu addItem:sep];
+	return sep;
+}
+
+- (void)removeSeparator:(NSMenuItem*)item
+{
+	[menu removeItem:item];
+}
+
+
+#pragma mark -
+#pragma mark WebScripting
+
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)sel {
+	return (nil == [self webScriptNameForSelector:sel]);
+}
+
++ (NSString *)webScriptNameForSelector:(SEL)sel 
+{
+	if (sel == @selector(setTitle:)) {
+		return @"setTitle";
+	}
+	else if (sel == @selector(getTitle)) {
+		return @"getTitle";
+	}
+	else if (sel == @selector(addItem:f:)) {
+		return @"addItem";
+	}
+	else if (sel == @selector(addSeparator)) {
+		return @"addSeparator";
+	}
+	else if (sel == @selector(removeSeparator:)) {
+		return @"removeSeparator";
+	}
+	return nil;
+}
+
+
++ (BOOL)isKeyExcludedFromWebScript:(const char*)key {
+	return (nil == [self webScriptNameForKey:key]);
+}
+
+
++ (NSString *)webScriptNameForKey:(const char *)name {
+	return nil;
 }
 
 
