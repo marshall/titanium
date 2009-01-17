a28a298b0a1693a213ac97d81dc9f9cb0fa1f679
jservin <servin7@gmail.com>
2009-01-16 22:47:24 -0600

    add a few methods for File object

--- modules/ti.Filesystem/file.cpp
+++ modules/ti.Filesystem/file.cpp
@@ -5,7 +5,8 @@
  */
 #include "File.h"
 
-#include "Poco/File.h"
+#include <Poco/File.h>
+#include <Poco/FileStream.h>
 #include <Poco/Exception.h>
 
 namespace ti
@@ -14,10 +15,20 @@ namespace ti
 	{
 		KR_ADDREF(global);
 
+		this->SetMethod("toString",&File::ToString);
 		this->SetMethod("isFile",&File::IsFile);
 		this->SetMethod("isDirectory",&File::IsDirectory);
 		this->SetMethod("isHidden",&File::IsHidden);
-		this->SetMethod("toString",&File::ToString);
+
+		this->SetMethod("isSymbolicLink",&File::IsSymbolicLink);
+		this->SetMethod("exists",&File::Exists);
+		this->SetMethod("read",&File::Read);
+		this->SetMethod("copy",&File::Copy);
+		this->SetMethod("move",&File::Move);
+		this->SetMethod("createDirectory",&File::CreateDirectoryX);
+		this->SetMethod("deleteDirectory",&File::DeleteDirectory);
+		this->SetMethod("deleteFile",&File::DeleteFileX);
+		this->SetMethod("getDirectoryListing",&File::GetDirectoryListing);
 	}
 
 	File::~File()
@@ -36,7 +47,7 @@ namespace ti
 		try
 		{
 			Poco::File file(this->filename);
-			isFile = file.exists() && file.isFile();
+			isFile = file.isFile();
 		}
 		catch (Poco::Exception& exc)
 		{
@@ -52,7 +63,7 @@ namespace ti
 		try
 		{
 			Poco::File dir(this->filename);
-			isDir = dir.exists() && dir.isDirectory();
+			isDir = dir.isDirectory();
 		}
 		catch (Poco::Exception& exc)
 		{
@@ -68,7 +79,7 @@ namespace ti
 		try
 		{
 			Poco::File file(this->filename);
-			isHidden = file.exists() && file.isHidden();
+			isHidden = file.isHidden();
 		}
 		catch (Poco::Exception& exc)
 		{
@@ -76,5 +87,230 @@ namespace ti
 		}
 
 		result->Set(isHidden);
+	}
+	void File::IsSymbolicLink(const ValueList& args, Value *result)
+	{
+		bool isLink = false;
+
+		try
+		{
+			Poco::File file(this->filename);
+			isLink = file.isLink();
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem getting file info:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(isLink);
+	}
+	void File::Exists(const ValueList& args, Value *result)
+	{
+		bool exists = false;
+
+		try
+		{
+			Poco::File file(this->filename);
+			exists = file.exists();
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem getting file info:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(exists);
+	}
+	void File::Read(const ValueList& args, Value *result)
+	{
+		try
+		{
+			Poco::File file(this->filename);
+			if(file.canRead())
+			{
+				Poco::FileInputStream fis(this->filename);
+
+				std::string contents;
+
+				while(! fis.eof())
+				{
+					std::string s;
+					std::getline(fis, s);
+
+					contents.append(s);
+				}
+
+				result->Set(contents);
+			}
+			else
+			{
+				result->SetNull();
+			}
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem reading file:::: " << exc.displayText() << std::endl;
+
+			result->SetNull();
+		}
+	}
+	void File::Copy(const ValueList& args, Value *result)
+	{
+		bool success = false;
+
+		try
+		{
+			std::string dest = args.at(0)->ToString();
+			// TODO need to verify parameters
+
+			Poco::File from(this->filename);
+
+			from.copyTo(dest);
+
+			success = true;
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem copying file:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(success);
+	}
+	void File::Move(const ValueList& args, Value *result)
+	{
+		bool success = false;
+
+		try
+		{
+			std::string dest = args.at(0)->ToString();
+			// TODO need to verify parameters
+
+			Poco::File from(this->filename);
+
+			from.moveTo(dest);
+
+			success = true;
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem moving file:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(success);
+	}
+	void File::CreateDirectoryX(const ValueList& args, Value *result)
+	{
+		bool created = false;
+
+		try
+		{
+			bool createParents = false;
+			if(args.size() > 0)
+			{
+				createParents = args.at(0)->ToBool();
+			}
+
+			Poco::File dir(this->filename);
+
+			if(! dir.exists())
+			{
+				if(createParents)
+				{
+					dir.createDirectories();
+					created = true;
+				}
+				else {
+					created = dir.createDirectory();
+				}
+			}
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem creating directory:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(created);
+	}
+	void File::DeleteDirectory(const ValueList& args, Value *result)
+	{
+		bool deleted = false;
+
+		try
+		{
+			bool deleteContents = false;
+			if(args.size() > 0)
+			{
+				deleteContents = args.at(0)->ToBool();
+			}
+
+			Poco::File dir(this->filename);
+
+			if(dir.exists() && dir.isDirectory())
+			{
+				dir.remove(deleteContents);
+
+				deleted = true;
+			}
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem deleting directory:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(deleted);
+	}
+	void File::DeleteFileX(const ValueList& args, Value *result)
+	{
+		bool deleted = false;
+
+		try
+		{
+			Poco::File file(this->filename);
+
+			if(file.exists() && file.isFile())
+			{
+				file.remove();
+
+				deleted = true;
+			}
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem deleting file:::: " << exc.displayText() << std::endl;
+		}
+
+		result->Set(deleted);
+	}
+	void File::GetDirectoryListing(const ValueList& args, Value *result)
+	{
+		try
+		{
+			Poco::File dir(this->filename);
+
+			if(dir.exists() && dir.isDirectory())
+			{
+				std::vector<std::string> files;
+				dir.list(files);
+
+				StaticBoundList *fileList = new StaticBoundList();
+
+				for(int i = 0; i < files.size(); i++)
+				{
+					Value *value = new Value(files.at(i));
+					fileList->Append(value);
+				}
+
+				result->Set(fileList);
+			}
+			else
+			{
+				result->SetNull();
+			}
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cerr << "Problem deleting file:::: " << exc.displayText() << std::endl;
+
+			result->SetNull();
+		}
 	}
 }
--- modules/ti.Filesystem/file.h
+++ modules/ti.Filesystem/file.h
@@ -7,6 +7,7 @@
 #define _TI_FILE_H_
 
 #include <api/binding/binding.h>
+#include <api/binding/static_bound_list.h>
 #include <string>
 
 namespace ti
@@ -21,10 +22,151 @@ namespace ti
 			BoundObject *global;
 			std::string filename;
 
-			void IsFile(const ValueList& args, Value *result);
-			void IsDirectory(const ValueList& args, Value *result);
-			void IsHidden(const ValueList& args, Value *result);
+			/**
+			 * Function: ToString
+			 *   Returns the File name
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   the file name
+			 */
 			void ToString(const ValueList& args, Value *result);
+			/**
+			 * Function: IsFile
+			 *   Determines if this File represents a file
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if this File repesents a file; false otherwise
+			 */
+			void IsFile(const ValueList& args, Value *result);
+			/**
+			 * Function: IsDirectory
+			 *   Determines if this File represents a directory
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if this File represents a directory; false otherwise
+			 */
+			void IsDirectory(const ValueList& args, Value *result);
+			/**
+			 * Function: IsHidden
+			 *   Determines if this File represents a hidden file or directory
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if this File is hidden; false otherwise
+			 */
+			void IsHidden(const ValueList& args, Value *result);
+			/**
+			 * Function: IsSymbolicLink
+			 *   Determines if this File represents a symbolic link
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if this File is a symbolic link; false otherwise
+			 */
+			void IsSymbolicLink(const ValueList& args, Value *result);
+			/**
+			 * Function: Exists
+			 *   Determines if this File represents an existing file or directory
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if this File exists; false otherwise
+			 */
+			void Exists(const ValueList& args, Value *result);
+			/**
+			 * Function: Read
+			 *   Reads the contents of this File
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   the text file contents as a string
+			 */
+			void Read(const ValueList& args, Value *result);
+			/**
+			 * Function: Copy
+			 *   Copies this file or directory to a given destination
+			 *
+			 *   If this File is a file, the file is copied to the destination specified
+			 *   If this File is a directory, the directory contents is recursively copied to the destination
+			 *
+			 * Parameters:
+			 *   destination - the destination where the file or directory is copied to
+			 *
+			 * Returns:
+			 *   true if the copy operation is successful; false otherwise
+			 */
+			void Copy(const ValueList& args, Value *result);
+			/**
+			 * Function: Move
+			 *   Moves this file or directory to the given destination
+			 *
+			 * Parameters:
+			 *   destination - the destination where the file or directory is moved to
+			 *
+			 * Returns:
+			 *   true if the move operation is successful; false otherwise
+			 */
+			void Move(const ValueList& args, Value *result);
+			/**
+			 * Function: CreateDirectory
+			 *   Creates this directory
+			 *
+			 * Parameters:
+			 *   recursive [= false] - controls if all parent directories are created if needed
+			 *
+			 * Returns:
+			 *   true if the directory is created; false otherwise
+			 */
+			void CreateDirectoryX(const ValueList& args, Value *result);
+			/**
+			 * Function: DeleteDirectory
+			 *   Deletes this directory
+			 *
+			 * Parameters:
+			 *   deleteContents [= false] - controls if the contents inside the directory are also deleted
+			 *
+			 * Returns:
+			 *   true if the directory is deleted; false otherwise
+			 */
+			void DeleteDirectory(const ValueList& args, Value *result);
+			/**
+			 * Function: DeleteFile
+			 *   Deletes this file
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if the file is deleted; false otherwise
+			 */
+			void DeleteFileX(const ValueList& args, Value *result);
+			/**
+			 * Function: GetDirectoryListing
+			 *   Gets a list of files/directories under this directory
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   An array of file/directory names under this directory.
+			 */
+			void GetDirectoryListing(const ValueList& args, Value *result);
+			/**
+			 * Function: Trash
+			 *
+			 * Parameters:
+			 *
+			 * Returns:
+			 *   true if the file or directory is moved to the trash; false otherwise
+			 */
 	};
 }
 
