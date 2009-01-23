a19256a7d600ce9306efc026c57bb43800dd7c89
Marshall Culpepper <marshall.law@gmail.com>
2009-01-22 18:12:57 -0600

    moved kroll to public git url, let's see if this fixes the build system. added some rake build scripts, (not working yet)

---
+++ build.rb
@@ -0,0 +1,52 @@
+#
+# Copyright 2008 Appcelerator, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+# merge titanium config into the mainline config for SDK
+TITANIUM_CONFIG = YAML::load_file(File.join(TITANIUM_DIR,'config.yml'))
+TITANIUM_TRANSPORT = S3Transport.new(DISTRO_BUCKET, TITANIUM_CONFIG)
+TITANIUM_MANIFEST = TITANIUM_TRANSPORT.manifest
+TITANIUM_CONFIG[:releases] = build_config(TITANIUM_CONFIG,TITANIUM_MANIFEST)
+merge_config(TITANIUM_CONFIG)
+
+
+namespace :titanium do
+
+  namespace :modules do
+    
+  end
+  
+  require File.join(TITANIUM_DIR,'project','build.rb')
+
+  task :dev do
+    Rake::Task["service:titanium"].invoke
+    Rake::Task["titanium:runtime:#{platform_string}"].invoke
+    Rake::Task["titanium:project"].invoke
+
+    ts = get_config(:service,:titanium)
+    system "app install:service #{ts[:output_filename]} --force"
+    
+    tp = get_config(:titanium,:project)
+    system "app install:titanium #{tp[:output_filename]} --force"
+    
+    tr = get_config(:titanium,platform_string.to_sym)
+    system "app install:titanium #{tr[:output_filename]} --force"
+  end
+
+  
+end
+
+
+
+
