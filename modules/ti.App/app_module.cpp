e4d759bb49d9666199bedcad2d43ed5be68ecac8
Jeff Haynie <jhaynie@gmail.com>
2009-01-31 19:08:09 -0800

    working on improving boot, installation

--- modules/ti.App/app_module.cpp
+++ modules/ti.App/app_module.cpp
@@ -17,8 +17,13 @@ namespace ti
 	void AppModule::Initialize()
 	{
 		const char *home = getenv("KR_HOME");
+		
+		
 		std::string config(home);
 		config+="/"CONFIG_FILENAME;
+
+		std::cout << "+++home = " << home << std::endl;
+		std::cout << "+++config = " << config << std::endl;
 
 		if (!FileUtils::IsFile(config))
 		{
