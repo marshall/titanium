f5f3a0d3c865677628e3797f5db717b7ab12094c
martin robinson <mrobinson@appcelerator.com>
2009-02-06 16:40:46 -0800

    Self-extracting/self-executing tar archive for Linux runtime installer

---
+++ installation/runtime/self_extracting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo Uncompressing archive...
+export TMPDIR=`mktemp -d /tmp/kXXXXXX`
+ARCHIVE=`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $0`
+tail -n+$ARCHIVE $0 | tar xz -C $TMPDIR
+
+CDIR=`pwd`
+cd $TMPDIR
+./titanium_runtime
+cd $CDIR
+rm -rf $TMPDIR
+
+exit 0
+__ARCHIVE_BELOW__
