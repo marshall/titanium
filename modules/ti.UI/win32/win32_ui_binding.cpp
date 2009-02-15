7d11b43c3e43b1503ebac956657b00ac63c75915
jservin <servin7@gmail.com>
2009-02-14 20:58:26 -0600

    implement app context menu in win32

--- modules/ti.UI/win32/win32_ui_binding.cpp
+++ modules/ti.UI/win32/win32_ui_binding.cpp
@@ -15,6 +15,8 @@
 
 namespace ti
 {
+	HMENU Win32UIBinding::contextMenuInUseHandle = NULL;
+
 	Win32UIBinding::Win32UIBinding(Host *host) : UIBinding(host)
 	{
 	}
@@ -44,8 +46,30 @@ namespace ti
 		}
 	}
 
-	void Win32UIBinding::SetContextMenu(SharedPtr<MenuItem>)
+	void Win32UIBinding::SetContextMenu(SharedPtr<MenuItem> menu)
 	{
+		SharedPtr<Win32MenuItemImpl> menu_new = menu.cast<Win32MenuItemImpl>();
+
+		// if same menu, just return
+		if ((menu.isNull() && contextMenuInUse.isNull()) || (menu_new == contextMenuInUse))
+		{
+			return;
+		}
+
+		// delete old menu if available
+		if(! contextMenuInUse.isNull())
+		{
+			contextMenuInUse->ClearRealization(contextMenuInUseHandle);
+			contextMenuInUseHandle = NULL;
+		}
+
+		contextMenuInUse = menu_new;
+
+		// create new menu if needed
+		if(! contextMenuInUse.isNull())
+		{
+			contextMenuInUseHandle = contextMenuInUse->GetMenu();
+		}
 	}
 
 	void Win32UIBinding::SetIcon(SharedString icon_path)
