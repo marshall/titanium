c25c16827a0e8b906118bd67a5965cc9977085c3
Jorge Servin <servin7@gmail.com>
2009-02-17 10:42:47 -0600

    fix compile issue in win32

--- modules/ti.Media/win32/win32_media_binding.cpp
+++ modules/ti.Media/win32/win32_media_binding.cpp
@@ -3,7 +3,7 @@
  * see LICENSE in the root folder for details on the license.
  * Copyright (c) 2008 Appcelerator, Inc. All Rights Reserved.
  */
-#include "win32_media.h"
+#include "win32_media_binding.h"
 
 namespace ti
 {
