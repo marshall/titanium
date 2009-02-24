3b1ad90adddfdea0a506e0cf90dddd2a52c560f9
Jeff Haynie <jhaynie@gmail.com>
2009-02-24 02:43:36 -0800

    product version comes from scons, fixed crash in process module

--- modules/ti.Process/process.cpp
+++ modules/ti.Process/process.cpp
@@ -50,42 +50,6 @@ namespace ti
 	Process::~Process()
 	{
 		Terminate();
-		
-		if (this->err)
-		{
-			delete this->err;
-			this->err = NULL;
-		}
-		if (this->out)
-		{
-			delete this->out;
-			this->out = NULL;
-		}
-		if (this->in)
-		{
-			delete this->in;
-			this->in = NULL;
-		}
-		if (this->errp)
-		{
-			delete this->errp;
-			this->errp = NULL;
-		}
-		if (this->outp)
-		{
-			delete this->outp;
-			this->outp = NULL;
-		}
-		if (this->inp)
-		{
-			delete this->inp;
-			this->inp = NULL;
-		}
-		if (this->process)
-		{
-			delete this->process;
-			this->process = NULL;
-		}
 	}
 	void Process::Terminate(const ValueList& args, SharedValue result)
 	{
