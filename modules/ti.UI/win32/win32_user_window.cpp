5cd89bb3510938b7e535ab5f1f30b9bf72f801a2
Jorge Servin <servin7@gmail.com>
2009-02-25 00:55:32 -0600

    inject appID into app:// url in win32s

--- modules/ti.UI/win32/win32_user_window.cpp
+++ modules/ti.UI/win32/win32_user_window.cpp
@@ -520,11 +520,59 @@ void Win32UserWindow::SetTitle(std::string& title) {
 	SetWindowText(window_handle, title.c_str());
 }
 
+std::string Win32UserWindow::NormalizeURL(std::string originalURL)
+{
+	// append <appID> if needed to the url
+
+	bool appurl = true;
+
+	std::string url(originalURL);
+	std::transform(url.begin(), url.end(), url.begin(), ::tolower);
+	if(url.find("http://") == 0 || url.find("https://") == 0)
+	{
+		appurl = false;
+	}
+
+	if(appurl)
+	{
+		// url will end up as app://<appID>/path
+		std::string appID = AppConfig::Instance()->GetAppID();
+
+		std::string urlPrefix("app://");
+		urlPrefix.append(appID);
+
+		if(url.find(urlPrefix) == 0)
+		{
+			// all good - nothing to do
+			url = originalURL;
+		}
+		else if(url.find("app://") == 0)
+		{
+			// need to add the <appID> .. keep one of the / characters
+			url = urlPrefix + originalURL.substr(5);
+		}
+		else if(url.find("/") == 0)
+		{
+			// need to add app://<appID>
+			url = urlPrefix + originalURL;
+		}
+		else
+		{
+			// need to add app://<appID>
+			// TODO - how do we handle relative URLs??
+			url = urlPrefix + "/" + originalURL;
+		}
+	}
+
+	return url;
+}
+
 void Win32UserWindow::SetURL(std::string& url_) {
 	std::string url = url_;
 
 	this->config->SetURL(url);
 
+	url = this->NormalizeURL(url);
 	std::cout << "SetURL: " << url << std::endl;
 
 	IWebMutableURLRequest* request = 0;
