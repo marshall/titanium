f5744993fdc9eb46f816af22939fe93676303a7a
martin robinson <mrobinson@appcelerator.com>
2009-03-27 11:02:54 -0700

    Packaging for Linux

--- support/linux/extractor.sh
+++ support/linux/extractor.sh
@@ -4,7 +4,7 @@ export TMPDIR=`mktemp -d /tmp/kXXXXXX`
 ARCHIVE=`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $0`
 tail -n+$ARCHIVE $0 | tar xz -C $TMPDIR
 
-$TMPDIR/titanium-sdk
+$TMPDIR/APPNAME
 rm -rf $TMPDIR
 
 exit 0
