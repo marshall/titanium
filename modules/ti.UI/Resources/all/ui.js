82d526ad65227725d06b215ee987f00623ccc4d8
Jeff Haynie <jhaynie@gmail.com>
2009-04-12 23:20:52 -0700

    run network status in detached thread, use anon function block for UI js

--- modules/ti.UI/Resources/all/ui.js
+++ modules/ti.UI/Resources/all/ui.js
@@ -3,31 +3,40 @@
  * http://license.appcelerator.com for full copy of the License.
  **/
 
- // A collection of JS patches for various UI functionality in Titanium
- 
-// Add app:// support to jquery's http success function
-if ('jQuery' in window) {
- 	var originalHttpSuccess = jQuery.httpSuccess;
-	jQuery.extend({
-		httpSuccess: function(r){
-			if (location.protocol == 'app:' && r.status === 0) {
-				return true;
-			}
-			return originalHttpSuccess.call(this,r);
-		}
-	}); 
- }
+// A collection of JS patches for various UI functionality in Titanium
 
-// adjust background transparency for window if needed
-if(Titanium) {
+//
+// execute in anonymous function block so now variables leak into the
+// global scope
+//
+(function()
+{
+	// Add app:// support to jquery's http success function
+	if (window.jQuery) 
+	{
+	 	var originalHttpSuccess = jQuery.httpSuccess;
+		jQuery.extend({
+			httpSuccess: function(r){
+				if (location.protocol == 'app:' && r.status === 0) {
+					return true;
+				}
+				return originalHttpSuccess.call(this,r);
+			}
+		}); 
+	 }
+
+	// adjust background transparency for window if needed
 	if(Titanium.platform == "win32") {
 		if(Titanium.UI.currentWindow.getTransparency() < 1) {
 			var c = Titanium.UI.currentWindow.getTransparencyColor();
 			document.body.style.background='#' + c;
 		}
 	}
-}
 
-// append the platform (osx, linux, win32) to the body so we can dynamically
-// use platform specific CSS such as body.win32 div { } 
-document.body.className = (document.body.className || '') + ' ' + Titanium.platform;
+	// append the platform (osx, linux, win32) to the body so we can dynamically
+	// use platform specific CSS such as body.win32 div { } 
+	var cn = (document.body.className || '');
+	document.body.className =  cn + (cn ? ' ': '') + Titanium.platform;
+	
+})();
+ 
