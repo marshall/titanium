66a04153c6f8eef36736eefb0053ce48d066206b
Jeff Haynie <jhaynie@gmail.com>
2009-05-27 20:29:20 -0700

    enable crash detection, cleanup messages and integration with backend, ability to test crash detection with scons build

--- modules/ti.Process/process_module.cpp
+++ modules/ti.Process/process_module.cpp
@@ -22,6 +22,13 @@ namespace ti
 		// set our ti.Process
 		SharedValue value = Value::NewObject(this->binding);
 		host->GetGlobalObject()->Set("Process", value);
+
+// simple test to make the app crash
+#ifdef TEST_CRASH_DETECTION		
+		char *s = NULL;
+		(*s)++;
+#endif
+		
 	}
 
 	void ProcessModule::Stop()
