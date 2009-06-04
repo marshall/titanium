1348dfa3f6c68aea59df9c79a203c74415c81686
Martin Robinson <mrobinson@appcelerator.com>
2009-06-04 14:44:32 -0700

    Clean up Process and remove deadlocks.
    All pipe output is now buffered and buffers are protected
    by mutexes. If an onread is attached after process exit it
    is always fired if there was no previous one and the buffer
    is cleared. The same now goes for onexit. Also fixed some
    potential uncaught exception situations in Pipe.

--- apps/drillbit/Resources/tests/process/process.js
+++ apps/drillbit/Resources/tests/process/process.js
@@ -17,20 +17,15 @@ describe("process tests",
 		var timer = null;
 		value_of(p).should_not_be_null();
 		var output = '';
-		
+
 		p.onread = function(buf)
 		{
 			output += buf;
 		};
+
 		p.onexit = function()
 		{
 			clearTimeout(timer);
-			// this can happen in cases where we exit before we 
-			// get everything out of the buffer
-			if (output.length==0)
-			{
-				output = p.out.read();
-			}
 			if (output.length > 0)
 			{
 				test.passed();
