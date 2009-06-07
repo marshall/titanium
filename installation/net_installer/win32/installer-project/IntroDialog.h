c4e49ed06f23bc6d05e020f02b90d16de396c5b0
Marshall Culpepper <mculpepper@appcelerator.org>
2009-06-06 19:25:32 -0700

    new update dialog for win net installer, use more information in each job.. some code separation, and more visual feedback in the copy/unzip/download progress indicator

--- installation/net_installer/win32/installer-project/IntroDialog.h
+++ installation/net_installer/win32/installer-project/IntroDialog.h
@@ -1,1 +1,34 @@
-BOOL CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
+#ifndef TI_INTRO_DIALOG_H
+#define TI_INTRO_DIALOG_H
+
+#include "Dialog.h"
+
+namespace ti
+{
+	class IntroDialog : public Dialog
+	{
+		protected:
+			HWND nameLabel;
+			HWND versionLabel;
+			HWND publisherLabel;
+			HWND urlLabel;
+			HWND licenseBlurb;
+			HWND licenseTextBox;
+			HWND installBox;
+			HWND installLocationText;
+			HWND installLocationButton;
+			HWND startMenuCheck;
+			HWND securityBlurb;
+			HWND runButton;
+			HWND cancelButton;
+
+			void InstallLocationClicked();
+			virtual void Initialize(HWND window);
+			virtual void ButtonClicked(int id);
+			
+		public:
+			IntroDialog();
+			
+	};
+}
+#endif
